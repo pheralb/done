12e27963fec421422e89272317c2a47b36d12032
pheralb <pablohdez4340@gmail.com>
2022-06-17 16:48:25 +0100

    🎉 Initial stitches config

---
+++ stitches.config.ts
@@ -0,0 +1,50 @@
+import { createStitches } from "@stitches/react";
+import {
+  gray,
+  blue,
+  red,
+  green,
+  grayDark,
+  blueDark,
+  redDark,
+  greenDark,
+} from "@radix-ui/colors";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      ...gray,
+      ...blue,
+      ...red,
+      ...green,
+    },
+  },
+  media: {
+    bp1: "(min-width: 480px)",
+  },
+  utils: {
+    marginX: (value: number) => ({ marginLeft: value, marginRight: value }),
+  },
+});
+
+// Dark Theme:
+const darkTheme = createTheme({
+  colors: {
+    ...grayDark,
+    ...blueDark,
+    ...redDark,
+    ...greenDark,
+  },
+});
+
+// Global CSS:
+
