75aa5eb1c791318eb060989c9f52c9a8653ed3da
pheralb <pablohdez4340@gmail.com>
2022-06-17 16:49:10 +0100

    ⚒️ Initial button style

---
+++ src/components/button/style.ts
@@ -0,0 +1,8 @@
+import { styled } from "stitches.config";
+
+export const ButtonStyled = styled("button", {
+  display: "inline-block",
+  padding: "5px 10px",
+  border: "none",
+  borderRadius: "5px",
+});
