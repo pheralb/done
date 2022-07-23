2d5aa643fbeafc2a3a5ba115a81dd28cd3df609d
pheralb <pablohdez4340@gmail.com>
2022-07-23 14:20:01 +0100

    ⚒️ Fix supabase.ts client.

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
+
 // Create Supabase Client ->
-export const supabaseClient = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
-);
+export const supabaseClient = createClient(supabaseUrl as string, supabaseKey as string);
